14504c8dc2695237f7f588603d7f1f55dd8b51c4
Melan Rashitha Dias <it17006712@my.sliit.lk>
2021-05-26 08:42:57 +0530

    axios login routes added

--- webservice/frontend/battery-broadcaster/src/components/LoginForm.jsx
+++ webservice/frontend/battery-broadcaster/src/components/LoginForm.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
+import axios from "axios";
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <center style={{ marginTop: "60px" }}>
-        <TextField id="standard-basic" label="Login name" />
+        <TextField
+          id="standard-basic"
+          onKeyUp={(e) => {
+            setUsername(e.target.value);
+            console.log(username);
+          }}
+          label="Login name"
+        />
         <br />
-        <TextField id="standard-basic" label="Password" />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          onKeyUp={(e) => {
+            setPassword(e.target.value);
+            console.log(password);
+          }}
+        />
         <br />
         <Button
           style={{ color: "white", background: "blue", marginTop: "15px" }}
+          onClick={() => {
+            axios.post("http://localhost:5000/login", {
+              username: username,
+              password: password,
+            });
+          }}
         >
           Login
         </Button>
