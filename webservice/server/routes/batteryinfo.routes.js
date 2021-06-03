ac2849f04b57ebd148185279f4054c980cec2967
Melan Rashitha Dias <it17006712@my.sliit.lk>
2021-06-03 16:06:05 +0530

    Implementation of getlatest based on timestamps

--- webservice/server/routes/batteryinfo.routes.js
+++ webservice/server/routes/batteryinfo.routes.js
@@ -49,4 +49,23 @@ BatteryRoute.get("/:id", async (req, res) => {
   }
 });
 
+BatteryRoute.get("/getlatest/:id", async (req, res) => {
+  console.log("Request captured with ID " + req.params.id);
+  try {
+    await batteryinfomodel
+      .findOne({ uid: req.params.id })
+      .sort({ timestamp: -1 })
+      .exec((err, docs) => {
+        if (err) {
+          res.send({ error: true, message: err.message }).status(400);
+          res.end();
+        } else {
+          res.send({ error: false, message: docs }).status(200);
+        }
+      });
+  } catch (ex) {
+    res.send({ error: true, message: ex.message }).status(400);
+  }
+});
+
 module.exports = BatteryRoute;
